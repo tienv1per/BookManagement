58722a967a38a571246b9af95ab5a164b0541491
tienv1per <tienv1perds@gmail.com>
2023-10-15 21:56:06 +0700

    add users model

---
+++ models/users.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        }, 
+        name: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        isAdmin: {
+            type: Boolean,
+            default: false,
+        },
+        phone: {
+            type: String,
+        },
+        address: {
+            type: String,
+        }
+    }, {timestamp: true}
+)
+
+module.exports = mongoose.model("Users", UserSchema);
