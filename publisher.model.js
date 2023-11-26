290596d18a42ecb35bb45c5da99568e2fa18ebc7
tienv1per <tienv1perds@gmail.com>
2023-11-26 20:33:47 +0700

    push

--- publisher.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const publisher = new Schema( {
-    name: {
-        type: String,
-        require: true
-    }
-});
-module.exports = mongoose.model('publisher', publisher);
