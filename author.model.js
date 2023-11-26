9ccb35203100dea71e592b74dbae5cbc7d1a3a00
tienv1per <tienv1perds@gmail.com>
2023-11-26 20:29:33 +0700

    up

--- author.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const author = new Schema({
-    name: {
-        type: String,
-        require: true,
-    }
-});
-module.exports = mongoose.model('author', author);
