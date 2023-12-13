6aef4ca9a3ec2c6a801f8c0efd92402afe28cc74
nguyen-van-tien0601 <108468086+nguyen-van-tien0601@users.noreply.github.com>
2023-12-13 08:35:40 +0700

    Update book.router.js

--- book.router.js
+++ book.router.js
@@ -3,7 +3,7 @@ const book_controller = require("../controllers/book.controller");
 
 const router = express.Router();
 
-router.post("/allbook", book_controller.getAllBook);
+router.get("/allbook", book_controller.getAllBook);
 router.post("/category", book_controller.getBookByCategory);
 router.post("/author", book_controller.getBookByAuthor);
 router.get("/:id", book_controller.getBookByID);
