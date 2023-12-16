8266ccb582c9eb7dddff54d193e5551ed8acd0d0
nguyen-van-tien0601 <anhtien06012002@gmail.com>
2023-12-16 17:32:59 +0700

    change

--- routes/bill.router.js
+++ routes/bill.router.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const router = express.Router();
-const bill_controller = require('../controllers/bill.controller');
+// const express = require('express');
+// const router = express.Router();
+// const bill_controller = require('../controllers/bill.controller');
 
-router.post("/add", bill_controller.addBill);
-router.get("/verify/:token", bill_controller.verifyPayment);
-router.get("/:id_user", bill_controller.getBillByIDUser);
-router.get("/delete/:id", bill_controller.deleteBill);
-router.get("/top10", bill_controller.statisticalTop10);
-router.post("/statistical/revenue/day", bill_controller.statisticaRevenueDay);
-router.post("/statistical/revenue/month", bill_controller.statisticaRevenueMonth);
-router.post("/statistical/revenue/year", bill_controller.statisticaRevenueYear);
-// router.post("/statistical/revenue/quauter", bill_controller.statisticaRevenueQuauter);
-router.get("/status/99", bill_controller.getBillNoVerify);
-router.get("/status/1", bill_controller.getBillVerify);
-router.get("/status/0", bill_controller.getProcessing);
-router.post("/updateissend", bill_controller.updateIssend);
+// router.post("/add", bill_controller.addBill);
+// router.get("/verify/:token", bill_controller.verifyPayment);
+// router.get("/:id_user", bill_controller.getBillByIDUser);
+// router.get("/delete/:id", bill_controller.deleteBill);
+// router.get("/top10", bill_controller.statisticalTop10);
+// router.post("/statistical/revenue/day", bill_controller.statisticaRevenueDay);
+// router.post("/statistical/revenue/month", bill_controller.statisticaRevenueMonth);
+// router.post("/statistical/revenue/year", bill_controller.statisticaRevenueYear);
+// // router.post("/statistical/revenue/quauter", bill_controller.statisticaRevenueQuauter);
+// router.get("/status/99", bill_controller.getBillNoVerify);
+// router.get("/status/1", bill_controller.getBillVerify);
+// router.get("/status/0", bill_controller.getProcessing);
+// router.post("/updateissend", bill_controller.updateIssend);
 
 
-module.exports = router;
+// module.exports = router;
