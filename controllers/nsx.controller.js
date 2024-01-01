3bb0066b9d15ed4e78ffd08cc509797ddfc8892a
nguyen-van-tien0601 <anhtien06012002@gmail.com>
2024-01-01 11:18:38 +0700

    change nsx

--- controllers/nsx.controller.js
+++ controllers/nsx.controller.js
@@ -12,3 +12,57 @@ exports.getIDBySearchText = async (searchText) => {
         throw err;
     }
 }
+
+exports.getNsx = async (req, res) => {
+    try {
+        const docs = await nsx.find({});
+
+        if (docs.length === 0) {
+            res.status(404).json({ data: [] });
+        } else {
+            res.status(200).json({ data: docs });
+        }
+    } catch (err) {
+        console.error('Error in find:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+exports.getAll = async (req, res) => {
+    nsx.find({})
+    .exec((err, docs) => {
+        if(err){
+            console.log(err);
+            res.status(500).json({msg: err});
+            return;
+        }
+        res.status(200).json({data: docs});
+    })
+}
+
+exports.getNameByID = async(req, res) => {
+    if(req.params.id === 'undefined') {
+        res.status(422).json({msg: 'Invalid data'})
+        return;
+    }
+    let result
+    try {
+        result = await nsx.findById(req.params.id)
+    }
+    catch(err) {
+        console.log(err);
+        res.status(500).json({msg: 'not found'})
+        return;
+    }
+    if(result === null){
+        res.status(404).json({msg: 'not found'});
+        return;
+    }
+    res.status(200).json({name: result.name});
+}
+
+
+
+
+
+
